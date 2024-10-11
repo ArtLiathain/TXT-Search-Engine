fff888a20137e6bc2ecd79a1418870e8a5f80975
eoghano4321 <eoghano4321@gmail.com>
2024-10-11 12:10:31 +0100

    feat: add hashmap implementation and header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../include/fileReader.h"
+#include "../include/hashmap.h"
 using namespace std;
 int main(int argc, char const *argv[])
 {
@@ -9,5 +10,16 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < books.length; i++){
         reader.readFile(books.get(i));
     }
+
+    Hashmap map;
+    vector<pair<string, Value>> keyValuePairs = {
+        make_pair("Apple", Value{10, "Book A"}),
+        make_pair("Banana", Value{5, "Book B"}),
+        make_pair("Strawberry", Value{5, "Book C"}),
+        make_pair("Orange", Value{3, "Book D"}),
+        make_pair("Grapes", Value{7, "Book E"})
+    };
+    map.createHashTable(keyValuePairs);
+    std::cout << "Apple found in " << map.getValue("Apple").book << " " << map.getValue("Apple").count << " times" << endl;
     return 0;
 }
