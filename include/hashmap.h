216c8137915ce9ec01cf813fd0f5433f25e144da
eoghano4321 <eoghano4321@gmail.com>
2024-10-13 13:37:07 +0100

    feat: Make hashmap implementation templated

--- include/hashmap.h
+++ include/hashmap.h
@@ -2,6 +2,8 @@
 #define HASHMAP_H
 
 #include <bits/stdc++.h>
+#include "./arraylist.h"
+#include <optional>
 
 #include <vector>
 #include <utility> // For std::pair
@@ -17,37 +19,316 @@ struct Value
     Value() : count(-1), book("") {}
 
     Value(int n, const string &t) : count(n), book(t) {}
+
+    bool operator!=(const Value &other) const {
+        return count != other.count || book != other.book;
+    }
+
+    bool operator==(const Value &other) const {
+        return count == other.count && book == other.book;
+    }
+
+    friend std::ostream& operator<<(std::ostream &os, const Value &v) {
+        os << "{count: " << v.count << ", book: \"" << v.book << "\"}";
+        return os;
+    }
 };
 
 // Structure to hold key-value pairs
+template <typename K, typename T>
 struct KeyValue
 {
-    string key;
-    Value value;
+    K key;
+    optional<T> value;
 
-    KeyValue() : key(""), value() {} // Default constructor, value set to -1 to indicate empty
+    KeyValue() : key(""), value(nullopt) {}
 };
 
-// Amount of versions of the hash function we want to use, most common amount in cuckoo hashing is 2
-#define ver 2
-
-class Hashmap
+template <typename K, typename T>
+class hashmap
 {
 private:
-    int hashKey(int function, const string &key);
     // Global variable for max number of buckets to be dynamically set based on the size of the input
     int MAXN;
-    KeyValue **hashtable;
-
+    // Amount of versions of the hash function. Standard for cuckoo hashing is 2
+    int ver; 
     // Array to store possible positions for a key
-    int pos[ver];
+    int *pos;
+
+    void rehash();
+    int hashKey(int function, const K &key);
 
 public:
+    KeyValue<K, T> **hashtable;
+    hashmap();
+    hashmap(int n);
+    hashmap(int n, int hash_versions);
+    ~hashmap();
     void initTable();
-    void place(const string &key, const Value &value, int tableID, int cnt, int n);
-    Value getValue(const string &key);
+    void add(const K &key, const T &value, int tableID, int cnt, int n);
+    void add(const K &key, const T &value);
+    T getValue(const K &key);
     void printTable();
-    void createHashTable(const vector<pair<string, Value>> &keyValuePairs);
+    void createHashTable(arraylist<pair<K, T>> &keyValuePairs);
 };
+
+template <typename K, typename T>
+hashmap<K, T>::hashmap(int n, int hash_versions)
+{
+    // TODO: Decide if this is sufficient or should I edit the hashkey code to allow for other types of keys
+    // Throw an error if key type is not convertable to a string
+    static_assert(std::is_convertible<K, std::string>::value, 
+            "Key type K must be derived from or convertible to std::string");
+    MAXN = n;
+    ver = hash_versions;
+    pos = new int[ver];
+    hashtable = new KeyValue<K, T>*();
+    initTable();
+}
+
+template <typename K, typename T>
+hashmap<K, T>::hashmap(int n)
+{
+    static_assert(std::is_convertible<K, std::string>::value, 
+            "Key type K must be derived from or convertible to std::string");
+    MAXN = n;
+    ver = 2;
+    pos = new int[ver];
+    hashtable = new KeyValue<K, T>*();
+    initTable();
+}
+
+template <typename K, typename T>
+hashmap<K, T>::hashmap()
+{
+    static_assert(std::is_convertible<K, std::string>::value, 
+            "Key type K must be derived from or convertible to std::string");
+    MAXN = 1;
+    ver = 2;
+    pos = new int[ver];
+    hashtable = new KeyValue<K, T>*();
+    initTable();
+}
+
+/* Function to retrieve the value associated with a key
+ * key: Which key's value we want to retrieve */
+template <typename K, typename T>
+T hashmap<K, T>::getValue(const K &key)
+{
+    for (int i = 0; i < ver; i++)
+    {
+        int index = hashKey(i + 1, key);
+        if (hashtable[i][index].key == key)
+        {
+            return hashtable[i][index].value.value();
+        }
+    }
+    return T();
+}
+
+/* function to place a key-value pair in one of its possible positions
+ * &key: Keyword being hashed
+ * &value: Value object containing the amount of ocurrences and the book title
+ * tableID: Which version of the table the key is being inserted into
+ * cnt: Number of displacements
+ * n: Amount of elements */
+template <typename K, typename T>
+void hashmap<K, T>::add(const K &key, const T &value, int tableID, int cnt, int n)
+{
+    if (cnt == n)
+    {
+        printf("%s unpositioned\n", key.c_str());
+        printf("Cycle present. REHASH.\n");
+        rehash();
+        add(key, value, tableID, cnt, n*2);
+        return;
+    }
+
+    for (int i = 0; i < ver; i++)
+    {
+        pos[i] = hashKey(i + 1, key);
+        if (hashtable[i][pos[i]].key == key)
+        {
+            hashtable[i][pos[i]].value = value;
+            return;
+        }
+    }
+
+    if (hashtable[tableID][pos[tableID]].value.has_value())
+    {
+        K displacedKey = hashtable[tableID][pos[tableID]].key;
+        T displacedValue = hashtable[tableID][pos[tableID]].value.value();
+
+        hashtable[tableID][pos[tableID]].key = key;
+        hashtable[tableID][pos[tableID]].value = value;
+
+        add(displacedKey, displacedValue, (tableID + 1) % ver, cnt + 1, n);
+    }
+    else
+    {
+        hashtable[tableID][pos[tableID]].key = key;
+        hashtable[tableID][pos[tableID]].value = value;
+    }
+}
+
+template <typename K, typename T>
+void hashmap<K, T>::add(const K &key, const T &value)
+{
+    int tableID = 0;
+    int cnt = 0;
+    int n = MAXN / 2;
+    if (cnt == n)
+    {
+        printf("%s unpositioned\n", key.c_str());
+        printf("Cycle present. REHASH.\n");
+        rehash();
+        add(key, value);
+        return;
+    }
+
+    for (int i = 0; i < ver; i++)
+    {
+        pos[i] = hashKey(i + 1, key);
+        if (hashtable[i][pos[i]].key == key)
+        {
+            hashtable[i][pos[i]].value = value;
+            return;
+        }
+    }
+
+    if (hashtable[tableID][pos[tableID]].value.has_value())
+    {
+        K displacedKey = hashtable[tableID][pos[tableID]].key;
+        T displacedValue = hashtable[tableID][pos[tableID]].value.value();
+
+        hashtable[tableID][pos[tableID]].key = key;
+        hashtable[tableID][pos[tableID]].value = value;
+
+        add(displacedKey, displacedValue, (tableID + 1) % ver, cnt + 1, n);
+    }
+    else
+    {
+        hashtable[tableID][pos[tableID]].key = key;
+        hashtable[tableID][pos[tableID]].value = value;
+    }
+}
+
+/* function to create a hashtable from an array list
+ * &keyValuePairs: Vector of key value pairs received from text rank to take in as input*/
+template <typename K, typename T>
+void hashmap<K, T>::createHashTable(arraylist<pair<K, T>> &keyValuePairs)
+{
+    int n = keyValuePairs.length;
+
+    for (int i = 0; i < n; i++)
+    {
+        add(keyValuePairs.get(i).first, keyValuePairs.get(i).second, 0, 0, n);
+    }
+
+    printTable();
+}
+
+/* function to print hash table contents */
+template <typename K, typename T>
+void hashmap<K, T>::printTable()
+{
+    printf("Final hash tables:\n");
+    for (int i = 0; i < ver; i++, printf("\n"))
+    {
+        for (int j = 0; j < MAXN; j++)
+        {
+            if (hashtable[i][j].value.has_value())
+                std::cout << hashtable[i][j].key.c_str() << ":" << hashtable[i][j].value.value() << " ";
+            else
+                printf("- ");
+        }
+    }
+    printf("\n");
+}
+
+/* function to fill hash table with dummy values for initialisation */
+template <typename K, typename T>
+void hashmap<K, T>::initTable()
+{
+    hashtable = new KeyValue<K, T>*[ver]; // Allocate rows for 'ver' tables
+    for (int i = 0; i < ver; i++)
+    {
+        hashtable[i] = new KeyValue<K, T>[MAXN]; // Allocate columns based on MAXN
+        for (int j = 0; j < MAXN; j++)
+        {
+            hashtable[i][j].value = nullopt; // Initialize to default
+        }
+    }
+}
+
+/* Simple hash function for strings
+ * function: Which version of the hash function we want to use
+ * &key: Reference to the keyword being hashed*/
+template <typename K, typename T>
+int hashmap<K, T>::hashKey(int function, const K &key)
+{
+    int hashValue = 0;
+    if (function == 1)
+    {
+        for (char c : key)
+        {
+            hashValue += c; // Simple character sum
+        }
+        return hashValue % MAXN;
+    }
+    else if (function == 2)
+    {
+        for (size_t i = 0; i < key.length(); i++)
+        {
+            hashValue += (i + 1) * key[i]; // Weighted character sum
+        }
+        return hashValue % MAXN;
+    }
+    else
+    {
+        throw std::invalid_argument("Invalid hash function version");
+    }
+}
+
+template <typename K, typename T>
+void hashmap<K, T>::rehash() {
+    int oldMAXN = MAXN; // Save the old size
+    MAXN *= 2; // Double the size
+    KeyValue<K, T>** oldTable = hashtable; // Keep reference to old table
+
+    // Initialize new hashtable
+    hashtable = new KeyValue<K, T>*[ver];
+    for (int i = 0; i < ver; i++) {
+        hashtable[i] = new KeyValue<K, T>[MAXN];
+        for (int j = 0; j < MAXN; j++) {
+            hashtable[i][j].value = nullopt; // Initialize to nullopt
+        }
+    }
+
+    // Rehash existing elements
+    for (int i = 0; i < ver; i++) {
+        for (int j = 0; j < oldMAXN; j++) {
+            if (oldTable[i][j].value.has_value()) { // Only rehash if there is a value
+                const K &key = oldTable[i][j].key;
+                const T &value = oldTable[i][j].value.value();
+
+                // Use the add function to insert the key-value pair into the new table
+                add(key, value, i, 0, oldMAXN); // Insert into the new hashtable
+            }
+        }
+        delete[] oldTable[i]; // Clean up old table row
+    }
+    delete[] oldTable; // Clean up old table
+}
+
+template <typename K, typename T>
+hashmap<K, T>::~hashmap() {
+    for (int i = 0; i < ver; i++) {
+        delete[] hashtable[i]; // Delete each row
+    }
+    delete[] hashtable; // Delete the array of row pointers
+    delete[] pos; // Delete the pos array
+}
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,34 @@ int main(int argc, char const *argv[])
         reader.readFile(books.get(i));
     }
 
-    Hashmap map;
-    vector<pair<string, Value>> keyValuePairs = {
-        make_pair("Apple", Value{10, "Book A"}),
-        make_pair("Banana", Value{5, "Book B"}),
-        make_pair("Strawberry", Value{5, "Book C"}),
-        make_pair("Orange", Value{3, "Book D"}),
-        make_pair("Grapes", Value{7, "Book E"})
-    };
+    arraylist<pair<string, Value>> keyValuePairs = arraylist<pair<string, Value>>(10);
+    keyValuePairs.insert(make_pair("Apple", Value{10, "Book A"}));
+    keyValuePairs.insert(make_pair("Banana", Value{5, "Book B"}));
+    keyValuePairs.insert(make_pair("Strawberry", Value{5, "Book C"}));
+    keyValuePairs.insert(make_pair("Orange", Value{3, "Book D"}));
+    keyValuePairs.insert(make_pair("Grapes", Value{7, "Book E"}));
+    hashmap<string, Value> map = hashmap<string, Value>(keyValuePairs.length, 2);
     map.createHashTable(keyValuePairs);
     std::cout << "Apple found in " << map.getValue("Apple").book << " " << map.getValue("Apple").count << " times" << endl;
+    std::cout << "Dog found in " << map.getValue("Dog").book << " " << map.getValue("Dog").count << " times" << endl;
+    
+    arraylist<pair<string, int>> keyIntPairs = arraylist<pair<string, int>>(3);
+    keyIntPairs.insert(make_pair("Apple", 12));
+    keyIntPairs.insert(make_pair("Banana", 6));
+    keyIntPairs.insert(make_pair("Strawberry", 5));
+    keyIntPairs.insert(make_pair("Orange", 7));
+    keyIntPairs.insert(make_pair("Grapes", 7));
+    keyIntPairs.insert(make_pair("Strawberry", 12));
+    hashmap<string, int> map2 = hashmap<string, int>(1);
+    map2.createHashTable(keyIntPairs);
+    std::cout << "Apple found " << map2.getValue("Apple") << " times" << endl;
+    std::cout << "Dog found " << map2.getValue("Dog") << " times" << endl;
+
+    // This currently throws a segmentation fault at the end of the run as it tries deleting map2 twice. Once when deleting doublemap and once when deleting map2
+    // hashmap<string, hashmap<string, int>> doublemap = hashmap<string, hashmap<string, int>>(2, 2);
+    // doublemap.add("Apple", &map2);
+    // std::cout << "Apple found in double layer map " << doublemap.getValue("Apple").getValue("Apple") << " times" << endl;
+    // std::cout << "Dog found in double layer map " << doublemap.getValue("Dog").getValue("Dog") << " times" << endl;
+
     return 0;
 }
