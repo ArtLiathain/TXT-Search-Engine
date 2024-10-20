bbf4ed97409fecc17ed71b8b45205eece31ff649
ArtLiathain <artp.oliathain@gmail.com>
2024-10-20 14:21:40 +0100

    feat: Add comments to merge sort

--- src/mergeSort.cpp
+++ src/mergeSort.cpp
@@ -11,6 +11,7 @@ mergeSort::~mergeSort()
 {
 }
 
+// Entrypoint function
 arraylist<pair<string, float>> mergeSort::sortArray(arraylist<pair<string, float>> array)
 {
     return breakUp(array, 0, array.length - 1);
@@ -19,14 +20,17 @@ arraylist<pair<string, float>> mergeSort::sortArray(arraylist<pair<string, float
 arraylist<pair<string, float>> mergeSort::breakUp(arraylist<pair<string, float>> array, int low, int high)
 {
 
+    // if high <= low a single value has been picked to be sorted and used as base for merge sort
     if (high <= low)
     {
-
+        // Create and return a single element array at low index
         arraylist<pair<string, float>> lowValueArr = arraylist<pair<string, float>>(1);
         lowValueArr.insert(array.get(low));
         return lowValueArr;
     }
+    // Get mid mpoint
     int mid = (high + low) / 2;
+    // recursive call with breakUp to get to single values to then merge up to the full array
     return merge(breakUp(array, low, mid), breakUp(array, mid + 1, high));
 }
 
@@ -38,10 +42,12 @@ arraylist<pair<string, float>> mergeSort::merge(arraylist<pair<string, float>> a
     int arr1Size = array1.length;
     int arr2Size = array2.length;
 
+    //Allocate new array with the exact capacity needed
     arraylist<pair<string, float>> newArr = arraylist<pair<string, float>>(arr1Size + arr2Size);
+    // Loop over until one pointer has has reached their capacity
     while (pointer1 < arr1Size && pointer2 < arr2Size)
     {
-
+        // insert the lowest value first and increment that sides pointer
         if (array1.get(pointer1).second < array2.get(pointer2).second)
         {
 
@@ -55,7 +61,7 @@ arraylist<pair<string, float>> mergeSort::merge(arraylist<pair<string, float>> a
             pointer2++;
         }
     }
-
+    // Append the rest of the values to the end of the combined array getting using all values
     while (pointer1 < arr1Size)
     {
         newArr.insert(array1.get(pointer1));
@@ -66,10 +72,6 @@ arraylist<pair<string, float>> mergeSort::merge(arraylist<pair<string, float>> a
         newArr.insert(array2.get(pointer2));
         pointer2++;
     }
-    for (int i = 0; i < newArr.length; i++)
-    {
-        cout << newArr.get(i).second << " ";
-    }
-    cout << endl;
+
     return newArr;
 }
